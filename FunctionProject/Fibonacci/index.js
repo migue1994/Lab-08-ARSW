428dca947f6c128a50cf643fbc0a5a2f0d700ccd
nduran06 <natalia.duran@mail.escuelaing.edu.co>
2020-04-22 00:44:31 -0500

    función recursiva

--- FunctionProject/Fibonacci/index.js
+++ FunctionProject/Fibonacci/index.js
@@ -9,19 +9,28 @@ module.exports = async function (context, req) {
 
     if (nth < 0)
         throw 'must be greater than 0'
-    else if (nth === 0)
-        answer = nth_2
-    else if (nth === 1)
-        answer = nth_1
-    else {
-        for (var i = 0; i < nth - 1; i++) {
-            answer = nth_2.add(nth_1)
-            nth_2 = nth_1
-            nth_1 = answer
-        }
+    else{
+        answer=fibonacci(nth)
     }
 
-    context.res = {
-        body: answer.toString()
-    };
+    context.res={
+        body:answer.toString()
+    };    
+}
+
+
+mem_values={
+    0: bigInt.zero,
+    1: bigInt.one
+}
+
+function fibonacci(nth){
+    if(nth in Object.keys(mem_values)){
+        return mem_values[nth];
+    }
+
+    else{
+        mem_values[nth]=fibonacci(nth-1).add(fibonacci(nth-2));
+        return mem_values[nth];
+    }
 }
